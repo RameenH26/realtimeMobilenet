ecc4c17760e3fc39bd0d84bbd56e1955ac3c699b
RameenH26 <76857653+RameenH26@users.noreply.github.com>
2021-07-27 21:04:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(480, 110);
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function draw() {
+ image(video, 0, 0, 300, 300);
+ classifier.classify(video, gotResult);
+}
+function modelLoaded() {
+  console.log("model is loaded");
+}
+
+function gotResult(error, results) {
+ if(error) {
+   console.log(error);
+ } else {
+   console.log(results);
+   document.getElementById("result_object_name").innerHTML = results[0].label;
+   document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+ }
+}
